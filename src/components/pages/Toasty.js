5d70eb68ba884dd2fd21257f91434c49e120ffe9
Rahul Mannepally <rahulmannepally@Rahuls-MacBook-Pro.local>
2023-03-26 09:47:31 +0530

    Added blog and inersection

---
+++ src/components/pages/Toasty.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import styles from './Toasty.module.css';
+
+function Toasty() {
+  const [isShown, setIsShown] = React.useState(false);
+
+  const wrapperRef = React.useRef();
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+
+      setIsShown(entry.isIntersecting);
+    });
+
+    observer.observe(wrapperRef.current);
+  }, []);
+
+  const translateX = isShown ? '0%' : '100%';
+
+  return (
+    <div ref={wrapperRef} className={styles.wrapper}>
+      <div
+        className={styles.character}
+        style={{
+          transform: `translateX(${translateX})`,
+        }}
+      >
+        🤩
+      </div>
+    </div>
+  );
+}
+
+export default Toasty;
